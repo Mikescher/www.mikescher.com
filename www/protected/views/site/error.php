b8d8e888d2b6fccdcbb1290fb204353e971565b6
Mike Schwörer <mailport@mikescher.de>
2014-05-13 18:25:37 +0200

    Started working on Navbar

--- www/protected/views/site/error.php
+++ www/protected/views/site/error.php
@@ -2,14 +2,14 @@
 /* @var $this SiteController */
 /* @var $error array */
 
-$this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
+	$this->pageTitle=Yii::app()->name . ' - Error';
+	
+	$this->breadcrumbs=array(
+		'Error',
+	);
+
+	$this->widget('bootstrap.widgets.TbHeroUnit', array(
+		'heading' => 'ERROR ' . $code,
+		'content' => $message,
+	));
 ?>
-
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
-</div>
--- www/protected/views/site/index.php
+++ www/protected/views/site/index.php
@@ -2,19 +2,83 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
+
+$this->breadcrumbs=
+	[
+		'Index'
+	];
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<div class="container">
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+	<!-- Main hero unit for a primary marketing message or call to action -->
+	<div class="hero-unit">
+		<h1>Hello, world!</h1>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+		<p> 
+		
+		<ul>
+			<li>View file: <code><?php echo __FILE__; ?></code></li>
+			<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+		</ul>
+		
+		</p>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+		<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+	</div>
+
+	<!-- Example row of columns -->
+	<div class="row">
+		<div class="span4">
+			<h2>Heading YD: <?php echo YII_DEBUG ?></h2>
+
+			<p>
+			
+			<?php
+				$connection = Yii::app()->db;
+				
+				$command=$connection->createCommand("SELECT * FROM programme");
+				$command->execute();   // a non-query SQL statement execution
+				// or execute an SQL query and fetch the result set
+				$reader=$command->query();
+				
+				// each $row is an array representing a row of data
+				$dbgtxt = "";
+				foreach($reader as $row)
+				{
+					$dbgtxt = $dbgtxt . print_r($row, true);
+				}
+				
+				echo TbHtml::textArea('dbgtxt', $dbgtxt, array('rows' => 5));
+			?>
+			
+			</p>
+
+			<p><a class="btn" href="#">View details &raquo;</a></p>
+		</div>
+		<div class="span4">
+			<h2>Heading</h2>
+
+			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+				condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+				euismod. Donec sed odio dui. </p>
+
+			<p><a class="btn" href="#">View details &raquo;</a></p>
+		</div>
+		<div class="span4">
+			<h2>Heading</h2>
+
+			<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+				porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+				fermentum massa justo sit amet risus.</p>
+
+			<p><a class="btn" href="#">View details &raquo;</a></p>
+		</div>
+	</div>
+
+	<hr>
+
+	<footer>
+		<p>&copy; Company 2012</p>
+	</footer>
+</div>
