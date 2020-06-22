ab17abc1c9ebedb898122532c7dacba28088c667
Mike Schwörer <pubgit@mikescher.com>
2020-06-22 22:39:12 +0200

    fix ipguard api call

--- www/commands/server_setselfaddress.php
+++ www/commands/server_setselfaddress.php
@@ -9,6 +9,6 @@ $ip = get_client_ip();
 
 file_put_contents(__DIR__ . '/../dynamic/self_ip_address.auto.cfg', $ip);
 
-system('add-trusted-ip "' . $ip . '"');
+system('ipguard add "' . $ip . '"');
 
 echo 'Ok.';
